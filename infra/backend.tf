e3d5bc69eb461a65a1ed8f5641460077c3a78f48
Kevin <klute001@gmail.com>
2026-05-30 13:23:40 -0700

    added s3 backend for remote state

--- infra/backend.tf
+++ infra/backend.tf
@@ -9,4 +9,12 @@ terraform {
       version = ">= 3.0.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "kevin-terraform-state-three-tier"
+    key            = "three-tier/terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "terraform-state-lock-three-tier"
+    encrypt        = true
+  }
 }
